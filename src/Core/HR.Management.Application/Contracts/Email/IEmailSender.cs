00f681bd0bcae24ce6b61eb23b5654b54f71b189
Pham Tien Dung <dungkbkbhn@gmail.com>
2023-09-21 21:57:08 +0700

    Add email service

---
+++ src/Core/HR.Management.Application/Contracts/Email/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using HR.Management.Application.Models.Email;
+
+namespace HR.Management.Application.Contracts.Email
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(EmailMessage email);
+    }
+}
---
+++ src/Infrastructure/HR.Management.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HR.Management.Application.Contracts.Email;
+using HR.Management.Application.Models.Email;
+using HR.Management.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.Management.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
